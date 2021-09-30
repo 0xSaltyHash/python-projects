34899326af19c542024071b222e9f7a7e6a779cf
ahmed <mohameda7med64@gmail.com>
2021-09-30 03:06:08 +0200

    Update User-Agent header

--- link-checker.py
+++ link-checker.py
@@ -19,10 +19,10 @@ if len(argv) != 2:
     exit(1)
 
 filename = argv[1]
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
               'Accept-Language': 'en-US,en;q=0.8',
-              'Accept-Encoding': 'gzip',}
+              'Accept-Encoding': 'gzip'}
 
 
 with open(filename, 'r') as f:
